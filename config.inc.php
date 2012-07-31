d1a7a74f42ba05461a3cefc0b248c29c1c031b26
Hirbod <Hirbod@hirbod.local>
2012-07-31 13:07:47 +0200

    small fixes in config.inc.php for better display in source

--- config.inc.php
+++ config.inc.php
@@ -2,7 +2,7 @@
 /**
  *  Medialist Sorter
  *
- * @version 1.0.0
+ * @version 1.0.1
  * @author Redaxo be_style plugin: Hirbod
  * @package redaxo 4.2.x and greater
  */
@@ -15,9 +15,10 @@ $REX['ADDON']['supportpage'][$mypage] = 'nightstomp.com';
 
 // INCLUDE MEDIALIST_SORTER JS/CSS
 $header = '
-<!-- medialist_sorter -->
-  <script src="../files/addons/be_style/plugins/medialist_sorter/script.js" type="text/javascript"></script>
-<!-- medialist_sorter -->
+
+  <!-- medialist_sorter -->
+    <script src="../files/addons/be_style/plugins/medialist_sorter/script.js" type="text/javascript"></script>
+  <!-- medialist_sorter -->
 ';
 $header_include = 'return $params["subject"].\''.$header.'\';';
 rex_register_extension('PAGE_HEADER', create_function('$params',$header_include));
